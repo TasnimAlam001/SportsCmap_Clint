eecdedaf27a1ae83eb5747d1168e428735f16ce8
tasnimalam001 <tasnimalam.001@gmail.com>
2023-06-13 16:59:36 +0600

    add class from created

--- src/Pages/Dashboard/Instructor/AddClass/AddClass.jsx
+++ src/Pages/Dashboard/Instructor/AddClass/AddClass.jsx
@@ -1,9 +1,133 @@
-
+import { useForm } from "react-hook-form";
+import useAuth from "../../../../Hooks/useAuth";
 
 const AddClass = () => {
+    const { user } = useAuth();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
     return (
-        <div>
-            <p>Add a class</p>
+        <div className="w-full">
+            <form onSubmit={handleSubmit(onSubmit)} className="w-full shadow-2xl rounded-lg m-5 ">
+                <div className="hero-content w-full flex-col lg:flex-row-reverse">
+                    <div className="card w-1/2 ">
+                        <div className="card-body w-full">
+                            <h1 className="text-center text-3xl font-extrabold">Add a class</h1>
+                            <div className="">
+
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text font-bold">Class Name</span>
+                                    </label>
+                                    <input type="text" placeholder="Class Name"  {...register("class_name", { required: true, maxLength: 24 })} className="input input-bordered" />
+                                    {errors.class_name && <span className="text-red-600 ">This field is required</span>}
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text font-bold">Class Image</span>
+                                    </label>
+                                    <input type="text" placeholder="Class Image"  {...register("class_image", { required: true })} className="input input-bordered" />
+                                    {errors.class_image && <span className="text-red-600">This field is required</span>}
+                                </div>
+
+                            </div>
+                            <div className="">
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text font-bold ">Instructor Name</span>
+                                    </label>
+                                    <input type="email" placeholder={user?.displayName} readOnly className="input input-bordered" />
+
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text font-bold ">Instructor Email</span>
+                                    </label>
+                                    <input type="email" placeholder={user?.email} readOnly className="input input-bordered" />
+
+                                </div>
+
+                            </div>
+                            <div className="grid md:grid-cols-2 gap-4">
+
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text font-bold ">Start Date</span>
+                                    </label>
+                                    <input type="date" placeholder="Date" {...register("date", { required: true })} className="input input-bordered" />
+                                    {errors.date && <span className="text-red-600">This field is required</span>}
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text font-bold ">Duration</span>
+                                    </label>
+                                    <input type="text" placeholder="Duration of the class" {...register("duration", { required: true })} className="input input-bordered" />
+                                    {errors.duration && <span className="text-red-600">This field is required</span>}
+                                </div>
+
+                            </div>
+                            <div className="grid md:grid-cols-2  gap-4">
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text font-bold ">Available Slots</span>
+                                    </label>
+                                    <input type="number" placeholder="Available Slots" {...register("slots", { required: true })} className="input input-bordered" />
+                                    {errors.slots && <span className="text-red-600">This field is required</span>}
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text font-bold ">Price</span>
+                                    </label>
+                                    <input type="number" placeholder="Price" {...register("price", { required: true })} className="input input-bordered" />
+                                    {errors.price && <span className="text-red-600">This field is required</span>}
+                                </div>
+                            </div>
+                            <div className="grid md:grid-cols-2  gap-4">
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text font-bold ">Age Range</span>
+                                    </label>
+                                    <input type="text" placeholder="Age Range" {...register("ageRange", { required: true })} className="input input-bordered" />
+                                    {errors.ageRange && <span className="text-red-600">This field is required</span>}
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text font-bold ">Available Slots</span>
+                                    </label>
+                                    <input type="number" placeholder="Available Slots" {...register("slots", { required: true })} className="input input-bordered" />
+                                    {errors.slots && <span className="text-red-600">This field is required</span>}
+                                </div>
+
+                            </div>
+
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-bold ">Location</span>
+                                </label>
+                                <input type="text" placeholder="Location" {...register("location", { required: true })} className="input input-bordered" />
+                                {errors.location && <span className="text-red-600">This field is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-bold ">Description</span>
+                                </label>
+                                <textarea type="text" placeholder="Description" {...register("description", { required: true })} className="input input-bordered h-20" />
+                                {errors.description && <span className="text-red-600">This field is required</span>}
+                            </div>
+                            
+
+
+                            <div className="form-control mt-6">
+
+                                <input className="btn btn-primary" type="submit" value="add class" />
+                            </div>
+
+
+                        </div>
+                    </div>
+
+                </div>
+            </form>
         </div>
     );
 };
